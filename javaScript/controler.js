609567ec210b8d0cc852ecde534d71de2b2411e8
Vinícius Gonzaga Guilherme <viniciusgonzagaguilherme@gmail.com>
2022-10-10 11:37:01 -0300

    att

--- javaScript/controler.js
+++ javaScript/controler.js
@@ -1,18 +1,30 @@
-
-
 var time = 0;
 
 function play(option){
+    let situation;
     let opt = document.getElementById(`opt${option}`);
     if(time % 2 == 0){
         opt.textContent = "X";
-        attGame(option, "X");
+        situation = attGame(option, "X");
     }
         
     else{
         opt.textContent = "O"
-        attGame(option, "O");
+        situation = attGame(option, "O");
     }
-        
+
+
+    endGame(situation);
+
     time++;
+}
+
+function endGame(sitation){
+    if(sitation != 0){
+        let opt1 = document.getElementById(`opt${sitation.opt1}`);
+        let opt2 = document.getElementById(`opt${sitation.opt2}`);
+        let opt3 = document.getElementById(`opt${sitation.opt3}`);
+
+        
+    }
 }
